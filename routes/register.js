7075faf11d37cfc7f131251289abd183c153905d
linyu-f <1274282051@qq.com>
2021-05-10 09:59:42 +0800

    5/10

--- routes/register.js
+++ routes/register.js
@@ -3,10 +3,21 @@ var router = express.Router();
 
 /* GET register listing. */
 router.get('/', function(req, res, next) {
-  res.send('这是注册界面Get');
+  let message = req.query;
+  // console.log(message);
+  let username = message.newusername;
+  let password = message.newpassword;
+  let feedBack = "你的用户名：" + username + "\n" + "你的密码：" + password;
+  res.send(feedBack);
 });
 
 router.post('/', function(req, res, next) {
+    // console.log(req.body);
+    let newUser = req.body;
+    let username = newUser.params.newusername;
+    let password = newUser.params.newpassword;
+    console.log(username);
+    console.log(password);
     res.send('这是注册界面Post');
   });
 
